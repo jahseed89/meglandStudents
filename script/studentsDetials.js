6a676e39c6ccea7bed3b199012c93c854a709455
Samson Ocran <samsonoc@futureconcerns.net>
2025-11-20 08:31:08 +0100

    updated students

--- script/studentsDetials.js
+++ script/studentsDetials.js
@@ -1,159 +1,150 @@
-
 let students = [
-    {
-        
-        name: "Oladimeji Paul Roland",
-        relationship: "######",
-        number: "08070995900",
-        location: "Nigeria",
-        // picture: "https://scontent.flos10-1.fna.fbcdn.net/v/t39.30808-6/215679393_4163431630418493_6263016040345060304_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeFJ9E_yuf1jCfjTS1rjamHpAxYjNrN6XHoDFiM2s3pceuYF6plk1R9uSBs6Jus2xq0HpKy594_y8nGB2nBmhhwK&_nc_ohc=-XqCCaEQmXoAX9l7C0o&_nc_ht=scontent.flos10-1.fna&oh=00_AT9s3Bu91e3bTQiMORdKmrghOBGda6jC84Kj3BCtfVSRXA&oe=62247CC1"
-    },
-    {
-        name: "Joke Ibk Lasore",
-        relationship: "######",
-        number: "XXXXXXXXX",
-        location: "Nigeria",
-        // picture: "https://scontent.flos10-2.fna.fbcdn.net/v/t1.18169-9/12189880_885967868147331_6435742391949215052_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeGRMeI2wCUTcJjeLp1PR3Hv75y1aXtYhETvnLVpe1iERLGsAMai_DxQ65lRs-FOb4XbW7MEMUatWjdhpJrfwa0k&_nc_ohc=bjsmiAAo1cwAX-yWaK5&_nc_ht=scontent.flos10-2.fna&oh=00_AT9FfpKijTXWdsCEyibvDBYMP5BTRz4Z0sbLqoZA_3JjYQ&oe=6242CE1C"
-    },
-    {
-        name: "Anita Mj",
-        relationship: "######",
-        number: "08062892146",
-        location: "Lagos",
-        // picture: "https://scontent.flos10-2.fna.fbcdn.net/v/t1.6435-9/202390689_5628329963909082_2790063894776769691_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeE4qDeLuRtEXrlHht_TawKfM_8UEiKlwVwz_xQSIqXBXAS9gC6YvRO-9t4_P_RvH9Efm3WF7_DF0rA9a7dciNAj&_nc_ohc=YwrOgPKsjpMAX-35ssY&_nc_ht=scontent.flos10-2.fna&oh=00_AT8D4QnJdJslq27dOV42iDsRXEw5LYK50Ullo1k5BlM2Hg&oe=62433384"
-     },
-      {
-        name: "Ethang",
-        relationship: "######",
-        number: "XXXXXXXXX",
-        location: "./images/ethang.jpg",
-        // picture: "https://scontent.flos10-2.fna.fbcdn.net/v/t1.6435-9/202390689_5628329963909082_2790063894776769691_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeE4qDeLuRtEXrlHht_TawKfM_8UEiKlwVwz_xQSIqXBXAS9gC6YvRO-9t4_P_RvH9Efm3WF7_DF0rA9a7dciNAj&_nc_ohc=YwrOgPKsjpMAX-35ssY&_nc_ht=scontent.flos10-2.fna&oh=00_AT8D4QnJdJslq27dOV42iDsRXEw5LYK50Ullo1k5BlM2Hg&oe=62433384"
-     },
-    {
-        name: "Dearest Bright C Eze",
-        relationship: "Single",
-        number: "XXXXXXXXX",
-        location: "Nigeria",
-        picture: "./images/donatus.jpg"
-     },
-    {
-        name: "Kehinde Omomobi",
-        relationship: "Maried",
-        number: "+233263559969",
-        location: "Ghana",
-        picture: "./images/kenny.jpg"
-    },
-    {
-        name: "Emmanuel Emugan",
-        relationship: "Single",
-        number: "XXXXXXXXXXX",
-        location: "Nigeria",
-        // picture: "https://scontent-los2-1.xx.fbcdn.net/v/t31.18172-8/24879579_1612708428793247_8716334171425672640_o.jpg?_nc_cat=101&ccb=1-5&_nc_sid=174925&_nc_eui2=AeEdHZtSR1j-cJ9USeUC8TOCx06TVtZhFM3HTpNW1mEUzeh3__J_wheoCNRWwYZTy2lkhW9F748kZdAsWaiKaNYA&_nc_ohc=m495Sohe7HsAX8TtDih&_nc_ht=scontent-los2-1.xx&oh=00_AT_QVqD23E1HOWkxXSVv3ad29j_Yz-XcKR6VkJshA1Nkvw&oe=624684DD"
-    },
-    {
-        name: "Gideon Philonis",
-        relationship: "Single",
-        number: "XXXXXXXXXXX",
-        location: "Nigeria",
-        // picture: "https://scontent-los2-1.xx.fbcdn.net/v/t39.30808-6/248657398_3967061136728202_7496735754295748288_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeGf6CH7u8M7Zu-wETxIk2-0dU9zbsdjuZp1T3Nux2O5mo-EVfqbJMrZ_J_e9aHGUP-4Sy7SUbUBDjfzMKy5Zgpk&_nc_ohc=ZwhI4IL1LbwAX_KcvNG&_nc_oc=AQmGmrr_GdaDv6Qsp8da8UQe1ohyg1RIV8TrlMX0ATo_km1FlrCDK4assHsb-SJ2UYU&_nc_ht=scontent-los2-1.xx&oh=00_AT80EHe2Zd_4kGrqYXAb3YqcWObWTupMgC9A8-ruSK9Gtg&oe=6224DEA6"
-    },
-    {
-        name: "Stella Solomon",
-        relationship: "Maried",
-        number: "09061519745",
-        location: "Lagos",
-        picture: "./images/stella.jpg"
-     },
-    {
-        name: "Emmanuel Kumson",
-        relationship: "Maried",
-        number: "XXXXXXXXX",
-        location: "Ghana",
-        picture: "./images/kumson.jpg"
-    },
-    {
-        name: "Balogun Dare",
-        relationship: "######",
-        number: "XXXXXXXXX",
-        location: "Nigeria",
-        // picture: "https://scontent.flos10-2.fna.fbcdn.net/v/t1.6435-9/103516331_3057736720977712_1090626348193455828_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHp5R9N3xC-XLwamyRWRUOru4dfaqAFZGa7h19qoAVkZvgB9DoOEqY_x5_LLeWUCnv6ElzaO9sHr-S0SACluUJH&_nc_ohc=hgklFdMzhlAAX9w3g7w&_nc_ht=scontent.flos10-2.fna&oh=00_AT8m5KFRuOtf72K-de4tkEr5kOELevz3rN2a6zwaumCVoQ&oe=62460AB6"
-    },
-    {
-        name: "Esther Nse",
-        relationship: "Maried",
-        number: "XXXXXXXXX",
-        location: "Lagos",
-        // picture: "https://scontent.flos10-1.fna.fbcdn.net/v/t1.6435-9/82360183_2692974254103795_4293339421790437376_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeH3S9uJmII7liHVjTnkTH0QDgu_mjF48j8OC7-aMXjyP1KENUAGoYkfR4NyO9YNRfEAOhiNNtoaRdz7GmhCgtg5&_nc_ohc=zfS6mSA7ZksAX9vAlBr&_nc_ht=scontent.flos10-1.fna&oh=00_AT9lmWzdNLeyWAsTtS7UCNcQKO29G-_y0UWSiyo_ID7Gcg&oe=6246C5C8"
-     },
-     {
-        name: "Orji Nneka Rosemary",
-        relationship: "Single",
-        number: "XXXXXXXXX",
-        location: "Lagos",
-        // picture: "https://scontent.flos10-1.fna.fbcdn.net/v/t1.6435-9/199835982_526250628550334_8681159686872795325_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=19026a&_nc_eui2=AeEwn2EhYGXJqELSUdC0rZ12Sqc5eQ-mpbxKpzl5D6alvDcx34MzyeC1ydJj2o5oJafP2c6473fY84iIpGXpu2xU&_nc_ohc=A7BsUHK3RA0AX-W8BTm&_nc_ht=scontent.flos10-1.fna&oh=00_AT_7xH4gkK-duWw07PPxY7hxi04aycQpmvDvGT1Fy0AvJw&oe=6247C93F"
-     },
-    {
-        name: "Samson Ocran",
-        relationship: "Single",
-        number: "+2348149835668",
-        location: "Lagos",
-        picture: "./images/sammy.jpg"
-     },
-    {
-        name: "Thomas Ogbor",
-        relationship: "Single",
-        number: "+XXXXXXXXXXX",
-        location: "Lagos",
-        // picture: "https://scontent.flos10-1.fna.fbcdn.net/v/t1.6435-9/170758932_4018990121494305_7625944948885854546_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeFjNfeCSADKKvCk2NfcpBo_9vd17LAT2j3293XssBPaPTF3zFJChHcoSwVBunRpBXFOkTZp_ocdXm73JEcQxKWy&_nc_ohc=Onp2oNkfGD4AX9BeLiF&_nc_ht=scontent.flos10-1.fna&oh=00_AT_RTxZXNye4eMdTAQLRtvJ4lZT6QcbyhE7zdNNhObh_Hw&oe=62437CB5"
-    },
-    {
-        name: "Gift Anthony",
-        relationship: "######",
-        number: "XXXXXXXXXXX",
-        location: "Lagos",
-        // picture: "https://scontent.flos10-1.fna.fbcdn.net/v/t1.6435-9/84189341_1870700263063698_274818129589174272_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=19026a&_nc_eui2=AeHI47gDAc9x4CBljKpkLog_7RnQ0vAPTentGdDS8A9N6dg6hc5IBLUmazue_DYm44ksvi0BZFn-RPHe181p-t9M&_nc_ohc=e54OgLQ5k7gAX8IsBVv&_nc_ht=scontent.flos10-1.fna&oh=00_AT9slm-0uMq4UOe5F-KUY3oQNbpZQixNTrPBYdfJVlYhKw&oe=624496BA"
-     },
-    {
-        name: "Dorathy Mathias Mfoniso",
-        relationship: "Maried",
-        number: "XXXXXXXXX",
-        location: "Lagos",
-        picture: "./images/dorathy.jpg"
-    },
-    {
-        name: "Annabel",
-        relationship: "Single",
-        number: "XXXXXXXXX",
-        location: "Lagos",
-        picture: "./images/hanna.jpg"
-    },
-    {
-        name: "Temmy Ogundele",
-        relationship: "######",
-        number: "XXXXXXXXX",
-        location: "Lagos",
-        // picture: "https://scontent.flos10-1.fna.fbcdn.net/v/t1.18169-9/14225587_1761731144065599_6284601804072036985_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=174925&_nc_eui2=AeFPAusfdasuNNWFOSRXp6SJV2yrxjcns05XbKvGNyezTsx8hjNrzzpmkxdUwcxuoO6TZYgYPHjC-w6ltDYVMXf_&_nc_ohc=ZSJy_URxoPEAX_JmAHQ&_nc_ht=scontent.flos10-1.fna&oh=00_AT-oG60F5F2gft8wRxJbuAGJiChkUJXWo8tpu0CMaWgLKA&oe=6245D9EA"
-    },
-    {
-        name: "Omolayo Esther",
-        relationship: "Maried",
-        number: "XXXXXXXXX",
-        location: "Lagos",
-        // picture: "https://scontent.flos10-2.fna.fbcdn.net/v/t1.18169-9/10350601_440625862741117_2864434785605976915_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHRx4Gab0NOKLowd6ODLsW7H3ZyGfUZMoEfdnIZ9RkygQTErvQDSENulLemjS-Z6xzKZFcMOkUZ4a9PvtZC84pp&_nc_ohc=YefVOOGi1j8AX-_IO1P&_nc_ht=scontent.flos10-2.fna&oh=00_AT9gWlA1mTHQd2nJo__jFYcopT6pScrNGbYJP_WTIGAU9A&oe=6243ACDE"
-    },
-    {
-        name: "Ajiboye Ibukunoluwa",
-        relationship: "######",
-        number: "XXXXXXXXX",
-        location: "Lagos",
-        // picture: "https://scontent.flos10-2.fna.fbcdn.net/v/t1.18169-9/1931307_905226659597438_7408001066876036882_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=174925&_nc_eui2=AeGD-L6LBWVJaqAaKiYUnKg1A8pTwl1r-TUDylPCXWv5NShnvRMd4BYKAeTR0VhajswLXh8wJXcUTX65tyswCU9-&_nc_ohc=_INDrUi9NCkAX8ekovd&tn=EVW5t-fg92ldgEtV&_nc_ht=scontent.flos10-2.fna&oh=00_AT8D5FVBQvf1nLofsFEb5xrG1k1l2LP90AAzzHMtt42CAg&oe=62465D00"
-    },
-     {
-        name: "Ayo",
-        relationship: "Maried",
-        number: "",
-        location: "Lagos",
-        picture:"./images/ayo.jpg"
-    }
-    
-    ]
+  {
+    name: "Oladimeji Paul Roland",
+    relationship: "######",
+    number: "08070995900",
+    location: "Nigeria",
+  },
+  {
+    name: "Joke Ibk Lasore",
+    relationship: "######",
+    number: "XXXXXXXXX",
+    location: "Nigeria",
+  },
+  {
+    name: "Anita Mj",
+    relationship: "######",
+    number: "08062892146",
+    location: "Lagos",
+  },
+  {
+    name: "Ethang",
+    relationship: "######",
+    number: "XXXXXXXXX",
+    location: "./images/ethang.jpg",
+    picture: "./images/ethan.jpg",
+  },
+  {
+    name: "Hakim",
+    relationship: "######",
+    number: "+226 55 75 52 56",
+    location: "./images/ethang.jpg",
+    picture: "./images/hakim.jpg",
+  },
+  {
+    name: "Dearest Bright C Eze",
+    relationship: "Single",
+    number: "XXXXXXXXX",
+    location: "Nigeria",
+    picture: "./images/donatus.jpg",
+  },
+  {
+    name: "Kehinde Omomobi",
+    relationship: "Maried",
+    number: "+233263559969",
+    location: "Ghana",
+    picture: "./images/kenny.jpg",
+  },
+  {
+    name: "Emmanuel Emugan",
+    relationship: "Single",
+    number: "XXXXXXXXXXX",
+    location: "Nigeria",
+  },
+  {
+    name: "Gideon Philonis",
+    relationship: "Single",
+    number: "XXXXXXXXXXX",
+    location: "Nigeria",
+  },
+  {
+    name: "Stella Solomon",
+    relationship: "Maried",
+    number: "09061519745",
+    location: "Lagos",
+    picture: "./images/stella.jpg",
+  },
+  {
+    name: "Emmanuel Kumson",
+    relationship: "Maried",
+    number: "XXXXXXXXX",
+    location: "Ghana",
+    picture: "./images/kumson.jpg",
+  },
+  {
+    name: "Balogun Dare",
+    relationship: "######",
+    number: "XXXXXXXXX",
+    location: "Nigeria",
+  },
+  {
+    name: "Esther Nse",
+    relationship: "Maried",
+    number: "XXXXXXXXX",
+    location: "Lagos",
+  },
+  {
+    name: "Orji Nneka Rosemary",
+    relationship: "Single",
+    number: "XXXXXXXXX",
+    location: "Lagos",
+  },
+  {
+    name: "Samson Ocran",
+    relationship: "Single",
+    number: "+2348149835668",
+    location: "Lagos",
+    picture: "./images/sammy.jpg",
+  },
+  {
+    name: "Thomas Ogbor",
+    relationship: "Single",
+    number: "+XXXXXXXXXXX",
+    location: "Lagos",
+  },
+  {
+    name: "Gift Anthony",
+    relationship: "######",
+    number: "XXXXXXXXXXX",
+    location: "Lagos",
+  },
+  {
+    name: "Dorathy Mathias Mfoniso",
+    relationship: "Maried",
+    number: "XXXXXXXXX",
+    location: "Lagos",
+    picture: "./images/dorathy.jpg",
+  },
+  {
+    name: "Annabel",
+    relationship: "Single",
+    number: "XXXXXXXXX",
+    location: "Lagos",
+    picture: "./images/hanna.jpg",
+  },
+  {
+    name: "Temmy Ogundele",
+    relationship: "######",
+    number: "XXXXXXXXX",
+    location: "Lagos",
+  },
+  {
+    name: "Omolayo Esther",
+    relationship: "Maried",
+    number: "XXXXXXXXX",
+    location: "Lagos",
+  },
+  {
+    name: "Ajiboye Ibukunoluwa",
+    relationship: "######",
+    number: "XXXXXXXXX",
+    location: "Lagos",
+  },
+  {
+    name: "Ayo",
+    relationship: "Maried",
+    number: "",
+    location: "Lagos",
+    picture: "./images/ayo.jpg",
+  },
+];
